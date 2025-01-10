8c0649e143b2be012cdaeda76c88196f4de682c1
manas <66103885+manasbir@users.noreply.github.com>
2025-01-10 18:31:57 -0500

    feat: enhance class scraping and data export to CSV

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scraper import Scraper
-import re
+import pandas as pd
 from helpers import format_class, add_query_params
 
 # mandatory classes
@@ -17,32 +17,42 @@ list_of_classes = []
 
 for c in classes:
     try:
-        list_of_info = []
         scraper = Scraper(add_query_params(class_info_url, c))
-        scraper.quit()
         lines = scraper.get_class_info().split("\n")[1:]
-        columns = [c.split(" ") for c in lines]
+        scraper.quit()
+        rows = [c.split(" ") for c in lines]
 
-        for col in columns:
-            if col[0].isdigit(): # col is good to check
-                if col[4] != "ONLINE":
-                    list_of_info.append({ "class": " ".join(c), "online": "no"})
-                list_of_info.append({ "class": " ".join(c), "online": "yes" ,"available": col[6]>col[7] })
+        for idx, row in enumerate(rows):
+            if row[0].isdigit():  # col is good to check
+                list_of_classes.append(["".join(c), row[4] == "ONLINE", row[6] > row[7]])
+            elif row[0] == "Reserve:" and row[1] == "Math":
+                list_of_classes.append(["".join(c),rows[idx-1][4] == "ONLINE",rows[idx-1][6] > rows[idx-1][7]])
             else:
+                print(f"Error with class {c}, \nError: {row}")
                 continue
-        if len(list_of_info) == 0:
-            list_of_classes.append({ "class": " ".join(c), "available": "err/not offered this semester" })
-        elif len(list_of_info) > 1:
-            for list in list_of_info:
-                if list["online"] == "yes":
-                    list_of_classes.append(list)
-                    break
-        else: 
-            list_of_classes.extend(list_of_info)
+
+        # if len(list_of_info) == 0:
+        #     list_of_classes.append(
+        #         {"class": "".join(c), "available": "err/not offered this semester"}
+        #     )
+        # # elif len(list_of_info) > 1:
+        # #     for row in list_of_info:
+        # #         if row["online"] == "yes":
+        # #             list_of_classes.append(row)
+        # #             break
+        # else:
+        #     list_of_classes.extend(list_of_info)
 
     except Exception as e:
         scraper.quit()
         print(f"Error with class {c}, \nError: {e}")
-        list_of_info.append({ "class": " ".join(c), "available": "err/not offered this semester" })
+        list_of_classes.append([" ".join(c), "err", "err"])
 
-print(list_of_info)
+df = pd.DataFrame(list_of_classes, columns=["class", "is online?", "is full?"])
+df.to_csv("classes.csv", index=False)
+# sort by online and then by full
+df = df.sort_values(["is online?", "is full?"], ascending=[False, True])
+df.to_csv("classes_sorted.csv", index=False)
+# filter by online
+df = df[df["is online?"] == True]
+df.to_csv("classes_online.csv", index=False)
--- scraper.py
+++ scraper.py
@@ -4,13 +4,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from helpers import format_class
+
 # constants
 # classes
 xpath_classes_list_1 = '//*[@id="__KUALI_TLP"]/div/div[2]/div[4]/span/div/div/div/div/div/div/section[2]/div/div[2]/section[1]/div/div/div/ul/li/div/div/ul'
 xpath_classes_list_2 = '//*[@id="__KUALI_TLP"]/div/div[2]/div[4]/span/div/div/div/div/div/div/section[2]/div/div[2]/section[2]/div/div/div/ul/li/ul/li[2]/div/div/ul'
 
-class_info_xpath = '/html/body/main/p[2]/table/tbody/tr[3]/td[2]/table/tbody'
-class_info_alt_xpath = '/html/body/main/p[2]/table/tbody/tr[3]/td[2]/table/tbody/tr[3]/td[2]/table/tbody'
+
+table_xpath = "/html/body/main/p[2]/table/tbody"
+class_info_links = [
+    "/html/body/main/p[2]/table/tbody/tr[3]/td[2]/table/tbody",
+    "/html/body/main/p[2]/table/tbody/tr[3]/td[2]/table/tbody/tr[3]/td[2]/table/tbody",
+    "/html/body/main/p[2]/table/tbody/tr[4]/td[2]/table/tbody",
+]
+
 
 class Scraper:
     def __init__(self, url):
@@ -27,16 +34,20 @@ class Scraper:
         except Exception as e:
             print(f"An error occurred: {e}")
             return None
-        
+
     def get_class_info(self):
         self.driver.get(self.url)
-        try: 
-            element = self.driver.find_element(By.XPATH, class_info_xpath)
-            return element.text
-        except Exception as e:
-            element = self.driver.find_element(By.XPATH, class_info_xpath)
-            return element.text
-        
+
+        self.driver.find_element(By.XPATH, table_xpath)
+
+        for xpath in class_info_links:
+            try:
+                element = self.driver.find_element(By.XPATH, xpath)
+                return element.text
+            except Exception as e:
+                print(f"err w link {self.url}")
+                continue
+
     def get_classes(self, use_cache=True):
         if use_cache:
             with open("classes.txt", "r") as f:
@@ -47,10 +58,8 @@ class Scraper:
 
             with open("classes.txt", "w") as f:
                 f.write("\n".join(classes))
-            
-            
+
         return [format_class(c) for c in classes]
-        
 
     def quit(self):
         self.driver.quit()
